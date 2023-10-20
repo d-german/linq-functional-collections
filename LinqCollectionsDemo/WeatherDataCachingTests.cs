df72986a279fe3678846bea9aaa3450af4cfbb9b
Damon German <damon.german@hyland.com>
2023-10-20 16:46:40 -0500

    Replace UnitTest1 with WeatherDataCachingTests
    UnitTest1.cs was deleted and replaced by WeatherDataCachingTests.cs, which includes new functionalities. WeatherDataCachingTests includes methods for caching verbose weather data in a dictionary and fetching it, with relevant unit tests included.

---
+++ LinqCollectionsDemo/WeatherDataCachingTests.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+
+namespace LinqCollectionsDemo;
+
+[TestFixture]
+public class WeatherDataCachingTests
+{
+    private static readonly IDictionary<string, string> WeatherCache = new Dictionary<string, string>();
+
+    private static string ExpensiveWeatherFetch(string location)
+    {
+        Thread.Sleep(2000);  // Simulate network delay
+        return $"Weather data for {location}: Sunny, 25°C";  // Simplified weather data
+    }
+
+    private static string GetWeatherData(string location)
+    {
+        if (!WeatherCache.ContainsKey(location))
+        {
+            WeatherCache[location] = ExpensiveWeatherFetch(location);
+        }
+
+        return WeatherCache[location];
+    }
+    
+    [Test]
+    public void FetchWeatherData_WithCache_Test()
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        // First fetch, data will be cached
+        var data1 = GetWeatherData("New York");
+        
+        stopwatch.Stop();
+        Console.WriteLine($"First fetch with cache took {stopwatch.ElapsedMilliseconds} ms.");
+        Assert.That(data1, Is.Not.Null);
+
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        // Second fetch, data will be retrieved from cache
+        var data2 = GetWeatherData("New York");
+
+        stopwatch.Stop();
+        Console.WriteLine($"Second fetch with cache took {stopwatch.ElapsedMilliseconds} ms.");
+        Assert.That(data2, Is.Not.Null);
+    }
+}
