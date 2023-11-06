1dac40dab11703818b690bc2ee3cc52d294b6337
Damon German <damon.german@hyland.com>
2023-11-06 13:10:59 -0600

    Refactor hash functions for clarity
    The CustomHashFunction, Insert, and Retrieve methods have been refactored to use a more verbose, but easier to understand code style. Instead of using complex single-line expressions, the logic is now expanded into multiple lines, making it easier to understand and debug.

--- LinqCollectionsDemo/StringHashTableUtility.cs
+++ LinqCollectionsDemo/StringHashTableUtility.cs
@@ -11,7 +11,16 @@ public static class StringHashTableUtility
     /// The hash value, computed as the sum of the ASCII values of the characters in the string,
     /// modulo the size of the hash table.
     /// </returns>
-    public static int CustomHashFunction(int hashSize, string value) => value.Sum(c => c) % hashSize;
+    public static int CustomHashFunction(int hashSize, string value)
+    {
+        var sum = 0;
+        for (var index = 0; index < value.Length; index++)
+        {
+            sum += value[index];
+        }
+
+        return sum % hashSize;
+    }
 
     /// <summary>
     /// Inserts a string value into a pre-sized hash table using a custom hash function.
@@ -22,7 +31,10 @@ public static class StringHashTableUtility
     /// This method computes the hash value for the string, and places the string at the corresponding index in the hash table.
     /// If there's already a value at the computed index, it will be replaced (collision resolution by overwriting).
     /// </remarks>
-    public static void Insert(this IList<string> values, string value) => values[CustomHashFunction(values.Count, value)] = value;
+    public static void Insert(this IList<string> values, string value)
+    {
+        values[CustomHashFunction(values.Count, value)] = value;
+    }
 
     /// <summary>
     /// Retrieves a string value from a hash table using a custom hash function.
@@ -36,5 +48,8 @@ public static class StringHashTableUtility
     /// This method computes the hash value for the string, and returns the string 
     /// at the corresponding index in the hash table if it matches the input value.
     /// </remarks>
-    public static string? Retrieve(this IList<string> values, string value) => (values[CustomHashFunction(values.Count, value)] == value ? value : null);
+    public static string? Retrieve(this IList<string> values, string value)
+    {
+        return values[CustomHashFunction(values.Count, value)] == value ? value : null;
+    }
 }
